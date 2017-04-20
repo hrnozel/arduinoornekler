1970e929b8b90f1c2f964a6ab17f7ddeae1be706
thirtyninekilovolts <harun.ozel91@gmail.com>
2017-04-20 16:25:19 +0300

    This example for how to long data write into eeprom and read

--- EEPROMKullanimi/EEPROMKullanimi.ino
+++ EEPROMKullanimi/EEPROMKullanimi.ino
@@ -1,16 +1,14 @@
-/*
- * EEPROM Write
- *
- * Stores values read from analog input 0 into the EEPROM.
- * These values will stay in the EEPROM when the board is
- * turned off and may be retrieved later by another sketch.
+/*Hello
+ * This example for how to long data (4 bit than more) write into eeprom and read
+ * Attention!
+ * Your Arduino's eeprom lifecycle can changed, 
+ * you would look your arduino's datasheet
  */
 
-#include <EEPROM.h>
+#include <EEPROMex.h>
 
-/** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
 int addr = 0;
-uint32_t phoneID[]={"5321231212","5331231111"};
+uint32_t phoneID=5331231111;
 uint32_t readVal=0;
 void setup() {
   Serial.begin(9600);
@@ -18,42 +16,13 @@ void setup() {
 
 void loop() {
  
-
-  /***
-    Write the value to the appropriate byte of the EEPROM.
-    these values will remain there when the board is
-    turned off.
-  ***/
-  for(int i=0;i<sizeof(phoneID)&& addr == EEPROM.length();i++){    
-  EEPROM.write(addr+i,phoneID[i]>>28);
-  }
- for(int i=0; i<EEPROM.length();i++){
-  readVal=EEPROM.read(i);
-  Serial.println(readVal<<28);}
-
-  /***
-    Advance to the next address, when at the end restart at the beginning.
-
-    Larger AVR processors have larger EEPROM sizes, E.g:
-    - Arduno Duemilanove: 512b EEPROM storage.
-    - Arduino Uno:        1kb EEPROM storage.
-    - Arduino Mega:       4kb EEPROM storage.
-
-    Rather than hard-coding the length, you should use the pre-provided length function.
-    This will make your code portable to all AVR processors.
-  ***/
+    
+  EEPROM.writeLong(addr,phoneID);
+  delay(1000);
  
-  if (addr == EEPROM.length()) {
-    addr = 0;
-  }
-
-  /***
-    As the EEPROM sizes are powers of two, wrapping (preventing overflow) of an
-    EEPROM address is also doable by a bitwise and of the length - 1.
-
-    ++addr &= EEPROM.length() - 1;
-  ***/
-
-
-  delay(100);
+  readVal=EEPROM.readLong(addr);
+  Serial.print("Your mom phone number: ");
+  Serial.println(readVal);
+  delay(10000);
+  
 }
